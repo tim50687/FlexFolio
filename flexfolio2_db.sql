48ff7255a5ec1471c19213e534823519967fb18f
hasankhwaja <128183942+hasankhwaja@users.noreply.github.com>
2023-11-13 21:11:05 -0500

    Update flexfolio2_db.sql

--- flexfolio2_db.sql
+++ flexfolio2_db.sql
@@ -25,50 +25,62 @@ create table post (
     date_posted datetime, 
     images blob, -- not sure yet 
     
-    -- added primary key of app_user and workout_group to satisfy ternary relationship
+    -- added primary key of app_user and workout_group to satisfy ternary relationship *resolved*
     user_email_fk varchar(64), 
     group_name_fk varchar(64),
-    primary key (user_email_fk, date_posted, group_name_fk),
     
+    primary key (user_email_fk, date_posted, group_name_fk),
     foreign key (user_email_fk) references app_user (user_email)
 		on update cascade on delete cascade,
 	foreign key (group_name_fk) references workout_group (group_name)
 		on update cascade on delete cascade
 );
 
+-- get the ppk from post and make it a ppk for user_comment *resolved*
+
 drop table if exists user_comment;
 create table user_comment (
 	comment_text tinytext,
     date_commented datetime,
     user_email_fk varchar(64), 
-    primary key (user_email_fk, date_commented),
+    date_posted_fk datetime,
+    group_name_fk varchar(64),
+    primary key (user_email_fk, date_commented, date_posted_fk, group_name_fk),
     foreign key (user_email_fk) references app_user (user_email)
+		on update cascade on delete cascade,
+	foreign key (group_name_fk) references workout_group (group_name)
+		on update cascade on delete cascade,
+	foreign key (user_email_fk, date_posted_fk, group_name_fk) references post (user_email_fk, date_posted, group_name_fk)
 		on update cascade on delete cascade
 );
+
+-- make attributes datetime and user email in headshot unique *resolved*
 
 drop table if exists headshot;
 create table headshot (
 	url varchar(128), -- is a picture not sure how to deal with this yet 
     date_headshot_uploaded datetime,
     user_email_fk varchar(64),
-    foreign key (user_email_fk) references app_user (user_email)
-	on update cascade on delete cascade
-);
-
-drop table if exists workouts;
-create table Workouts (
-	workout_id int primary key, 
-    excercise_name varchar(64), 
-    sets int, 
-    reps int, 
-    date_recorded datetime,
-    user_email_fk varchar(64),
+    unique(url, date_headshot_uploaded),
     foreign key (user_email_fk) references app_user (user_email)
 		on update cascade on delete cascade
 );
 
--- there should be 4 extra tables for ternary relationships: writes, gives, creates
--- and one more for the many to many relationship between user and group 
+-- make workouts a weak entity and use user_email as a fk and ppk *resolved*
+
+drop table if exists workouts;
+create table workouts ( 
+    exercise_name varchar(64), 
+    sets int, 
+    reps int, 
+    date_recorded datetime,
+    user_email_fk varchar(64),
+    primary key (user_email_fk, exercise_name),
+    foreign key (user_email_fk) references app_user (user_email)
+		on update cascade on delete cascade
+);
+
+-- there should be 4 extra tables for ternary relationships: writes, gives, creates and one more for the many to many relationship between user and group *resolved*
 
 drop table if exists user_writes;
 create table user_writes (
@@ -79,7 +91,7 @@ create table user_writes (
     primary key (user_email_ppk, date_posted_ppk, date_commented_ppk, group_name_ppk),
     foreign key (user_email_ppk, date_posted_ppk, group_name_ppk) references post (user_email_fk, date_posted, group_name_fk)
 		on update cascade on delete cascade,
-	foreign key (user_email_ppk, date_commented_ppk) references user_comment (user_email_fk, date_commented)
+	foreign key (user_email_ppk, date_commented_ppk, date_posted_ppk, group_name_ppk) references user_comment (user_email_fk, date_commented, date_posted_fk, group_name_fk)
 		on update cascade on delete cascade
 );
 
@@ -119,6 +131,3 @@ create table user_likes_post (
 	foreign key (user_email_ppk3, date_posted_ppk3, group_name_ppk3) references post (user_email_fk, date_posted, group_name_fk)
 		on update cascade on delete cascade
 );
-
-        
-        
