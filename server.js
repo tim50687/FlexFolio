324394fffcf0570789d67c8c7a6c08eae8544184
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2023-11-15 14:44:28 -0500

    connect to database

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require("express");
 const app = express(); // create express app
-const PORT = 3000;
+const PORT = 3000; // port where server is running
+
+// Set up environment variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Set up sequelize
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
+// Check databse connection (Promise-based approach)
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello, Flexfolio");
