21743f22ed8fb87c6c1c79b0a1276088da1436bf
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2023-12-04 23:14:13 -0500

    Made the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  // get the current user from the redux store
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,8 +18,16 @@ export default function Header() {
           <Link to="/">
             <li className="text-slate-700 hover:underline">Home</li>
           </Link>
-          <Link to="/sign-in">
-            <li className="text-slate-700 hover:underline">Sign In</li>
+          <Link to="/profile">
+            {currentUser ? ( // if there is a user
+              <img
+                className="rounded-full h-7 w-7"
+                src={currentUser.user_photo_url}
+                alt="Profile"
+              />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Profile() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form>
+        <img src="" alt="" />
+      </form>
+    </div>
+  );
 }
