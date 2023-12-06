02c562f515c6cebef4a39982a233872e2f8ee402
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2023-12-06 02:39:06 -0500

    updated home page and added home image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import "../../src/Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <section className="hero-section">
+        <h1>Unleash Your Potential with FlexFolio!</h1>
+
+        <img src="/fitness.png" alt="Fitness Inspiration" />
+      </section>
+    </div>
+  );
 }
